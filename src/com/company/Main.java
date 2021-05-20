83a40fe1a57a8a759ce1cbd121fe39046051489e
Luis <lanorman14@gmail.com>
2021-05-20 12:01:22 -0500

    Refactored

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,11 @@ public class Main {
 
         // Saga activities - this saga passes all activities
         ReserveCar reserveCar = new ReserveCar();
-        reserveCar.maxRand = 100; // since max rand num is 100 this will always pass
+        reserveCar.maxRand = 100; // since saga activity val num is 100 this will always pass
         BookHotel bookHotel = new BookHotel();
-        bookHotel.maxRand = 100; // since max rand num is 100 this will always pass
+        bookHotel.maxRand = 100; // since saga activity val is 100 this will always pass
         BookFlight bookFlight = new BookFlight();
-        bookFlight.maxRand = 100; // since max rand num is 100 this will always pass
+        bookFlight.maxRand = 100; // since saga activity val is 100 this will always pass
 
         // Note: if max rand is set at 50 that means there is a 50% chance of it passing
         // Use saga builder to build saga activities
@@ -30,11 +30,11 @@ public class Main {
         // This saga fails on the last activity (Book flight)
         // Saga activities - use diff objects
         ReserveCar reserveCar2 = new ReserveCar();
-        reserveCar2.maxRand = 100; // since max rand num is 100 this will always pass
+        reserveCar2.maxRand = 100; // since saga activity val num is 100 this will always pass
         BookHotel bookHotel2 = new BookHotel();
-        bookHotel2.maxRand = 100; // since max rand num is 100 this will always pass
+        bookHotel2.maxRand = 100; // since saga activity val is 100 this will always pass
         BookFlight bookFlight2 = new BookFlight();
-        bookFlight2.maxRand = -1; // since min rand num is 0 this will always pass
+        bookFlight2.maxRand = -1; // since min ran value is 0 this will always fail
 
         // Note: if max rand is set at 0 < it will always fail
         // Use saga builder to build saga activities
